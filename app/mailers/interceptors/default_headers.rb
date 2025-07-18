4a0b8da7b21161390b249046b20fc2619bc4ba3c
Alexander Brandon Coles <a.coles@openproject.com>
2025-07-18 17:36:20 +0100

    Freeze string literals in app/mailers
        rubocop -A --only Style/FrozenStringLiteralComment,Layout/EmptyLineAfterMagicComment,Style/RedundantFreeze app/mailers

--- app/mailers/interceptors/default_headers.rb
+++ app/mailers/interceptors/default_headers.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Interceptors
   module DefaultHeaders
     module_function
--- app/mailers/interceptors/do_not_send_mails_without_recipient.rb
+++ app/mailers/interceptors/do_not_send_mails_without_recipient.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Interceptors
   module DoNotSendMailsWithoutRecipient
     module_function
