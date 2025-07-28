9685c9030f1d1224f075008c6dd90c6994a63f98
Henriette Darge <h.darge@openproject.com>
2025-07-28 11:26:15 +0200

    Add `see_other` status to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -81,12 +81,12 @@ class CategoriesController < ApplicationController
     if @issue_count == 0
       # No issue assigned to this category
       @category.destroy
-      redirect_to project_settings_categories_path(@project)
+      redirect_to project_settings_categories_path(@project), status: :see_other
       return
     elsif params[:todo]
       reassign_to = @project.categories.find_by(id: params[:reassign_to_id]) if params[:todo] == "reassign"
       @category.destroy(reassign_to)
-      redirect_to project_settings_categories_path(@project)
+      redirect_to project_settings_categories_path(@project), status: :see_other
       return
     end
     @categories = @project.categories - [@category]
